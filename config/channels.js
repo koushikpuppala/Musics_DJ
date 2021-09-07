60febbabb50c73584c6aba526d6b84eb42675493
koushikpuppala <koushikpuppala203@gmail.com>
2021-09-07 10:09:02 +0530

    Musics_DJ

---
+++ config/channels.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'readyChannel': '814418207837388860',
+    'guildaddChannel': '814416971948883968',
+    'guildleftChannel': '814568063373803580',
+    'messageChannel': '779219247179366431',
+    'patreonChannel': '815120506150977597',
+    'voteChannel': '779219467644829698',
+    'shardChannel': '814568941421985843',
+    'reconnectChannel': '814569001832284160',
+    'warnChannel': '814569438124703785',
+    'disconnectChannel': '814569341839605760',
+    'errorChannel': '814569392293150760',
+};
---
+++ config/patreon.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'premiumPrice': '$15',
+    'proPrice': '$30',
+    'defaultMaxSongs': '1000',
+    'premiumMaxSongs': '5000',
+    'proMaxSongs': '10000',
+};
